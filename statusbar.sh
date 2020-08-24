eab89a911dba9e9a49a1136d8c6a2c31c0767234
lachlanalston <41813866+lachlanalston@users.noreply.github.com>
2020-08-25 03:34:00 +1000

    Update statusbar.sh

--- statusbar.sh
+++ statusbar.sh
@@ -24,40 +24,12 @@ bat(){
     fi
     printf "%s\n" "$SEP2"
 }
-
- alsa () {
-    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
-    printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$VOL" -eq 0 ]; then
-            printf "   ^=^t^g"
-        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "   ^=^t^h %s%%" "$VOL"
-        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "   ^=^t^i %s%%" "$VOL"
-        else
-            printf "   ^=^t^j %s%%" "$VOL"
-        fi
-    else
-        if [ "$VOL" -eq 0 ]; then
-            printf "MUTE"
-        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "VOL %s%%" "$VOL"
-        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "VOL %s%%" "$VOL"
-        else
-            printf "VOL %s%%" "$VOL"
-        fi
-    fi
-    printf "%s\n" "$SEP2"
-}
-
 wea(){
         LOCATION=""
         curl -s https://wttr.in/$LOCATION?format=1
 }
 
 while true; do
-        xsetroot -name "$(dte) | $(bat) | $(alsa) | $(wea)"
+        xsetroot -name "$(dte) | $(bat) | $(wea)"
         sleep 1s
 done
